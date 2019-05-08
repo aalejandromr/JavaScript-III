c8de167367a787e47e0c60a849bfa7994ad1ae2a
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-05-08 15:18:00 -0400

    Gets principle 1, 2, and 3 done for this.js [WIP]

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Implicit binding
+* 2. Explicit binding
+* 3. Global/Window
+* 4. When creatinig new classes
 *
 * write out a code example of each explanation above
 */
@@ -12,15 +12,39 @@
 // Principle 1
 
 // code example for Window Binding
+function showGlobal(){
+    console.log(this);
+}
 
+// showGlobal();
 // Principle 2
 
 // code example for Implicit Binding
 
+const me = {
+    name: "Alejandro",
+    age: 21,
+    getJob: function(){
+        console.log(`${this.name} got a job at the age of ${this.age}, thanks to Lambda`)
+    }
+}
+
+me.getJob();
 // Principle 3
 
 // code example for New Binding
+function me_f(name, age){
+    this.name = name;
+    this.age = age;
+    this.getJob = function(){
+        console.log(`${this.name} got a job at the age of ${this.age}, thanks to Lambda`);
+    }
+}
 
+const is_me = new me_f("Alejandro", 21);
+const is_my_bf = new me_f("Daniel", 21);
+is_me.getJob();
+is_my_bf.getJob();
 // Principle 4
 
 // code example for Explicit Binding
